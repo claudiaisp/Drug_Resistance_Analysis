ef982746cbfae69324c525111bb15b207d681387
claudiaisp <44985628+claudiaisp@users.noreply.github.com>
2020-05-01 15:40:10 +0200

    Update Data_Obtention_Script.sh

--- Data_Obtention_Script.sh
+++ Data_Obtention_Script.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-#for a in /media/claudia/Backup/claudia/Sequences/Assemblies/*
-#do	
-	#n1="/media/claudia/Backup/claudia/Sequences/Assemblies/"
-	#name1=${a#"$n1"}
-	#amrfinder -n "$a" -o "/media/claudia/Backup/claudia/Sequences/Finder/${name1}"
+for a in /"path were the files are"/*
+do	
+	n1="/"path were the files are"/"
+	name1=${a#"$n1"}
+	amrfinder -n "$a" -o "/media/claudia/Backup/claudia/Sequences/Finder/${name1}"
 	
-#done
+done
 
-for b in /media/claudia/Backup/claudia/Sequences/Assemblies/*
+for b in /"path were the files are"/*
 do
-	n2="/media/claudia/Backup/claudia/Sequences/Assemblies/"
+	n2="/"path were the files are"/"
 	name2=${b#"$n2"}
-	blastn -query $b -db megares_drugs_database -outfmt "6 qseqid sseqid slen length pident mismatch gapopen qstart qend sstart send evalue bitscore" -perc_identity 95.00 -out /media/claudia/Backup/claudia/Sequences/Megares/Drugs/${name2}
-	blastn -query $b -db megares_modified_database -outfmt "6 qseqid sseqid slen length pident mismatch gapopen qstart qend sstart send evalue bitscore" -perc_identity 95.00 -out /media/claudia/Backup/claudia/Sequences/Megares/Modified/${name2}
+	blastn -query $b -db megares_drugs_database -outfmt "6 qseqid sseqid slen length pident mismatch gapopen qstart qend sstart send evalue bitscore" -perc_identity 95.00 -out /"path where the files will be stored"/${name2}
+	blastn -query $b -db megares_modified_database -outfmt "6 qseqid sseqid slen length pident mismatch gapopen qstart qend sstart send evalue bitscore" -perc_identity 95.00 -out /"path where the files will be stored"/${name2}
 done
 
 
